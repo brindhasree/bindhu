757543cf2dcaae530ebe4919601a9908a7c51550
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-02-15 14:59:52 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n!=0)
+	{
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"hello"<<endl;
+	}
+	}
+	else 
+	cout<<"zero";
+	// your code goes here
+	return 0;
+}
