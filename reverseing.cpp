e202866b369d00ed6d80a78a82dcff1d8cb450f9
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-02 17:54:47 +0530

    Rename reverseint.cpp to reverseing.cpp

---
+++ reverseing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class reverse
+{
+	int n,r,rev=0;
+	public:
+	void work()
+	{
+		cin>>n;
+		while (n > 0)
+r = n % 10;
+rev = rev * 10 + r;
+cout<<rev;
+
+	}
+	
+};
+
+int main() {
+	 reverse r;
+	 r.work();
+	 
+
+	return 0;
+}
