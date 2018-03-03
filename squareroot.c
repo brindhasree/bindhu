307760b2580a690de679fd487bb0c9b49def6037
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-03 17:10:42 +0530

    Create squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+
+
+int main(void) {
+	int n,k,k1;
+	scanf("%d",&n);
+	scanf("%d",&k);
+	k1=sqrt(n);
+	if(k==k1)
+	
+		printf("yes");
+		else
+		printf("no");
+	
+	return 0;
+	getch();
+}
