a2545ed498c2390b66aebc3b5d7835043892ebb3
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-05 09:39:59 +0530

    Create smallestfromarray.c

---
+++ smallestfromarray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,a[100],n1;
+	scanf("%d",&n)
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	n1=n;
+	for(i=0;i<n;i++)
+	{
+		if(n1<a[i])
+		{n1=a[i];
+	
+	}
+	
+	// your code goes here
+	return 0;
+}
