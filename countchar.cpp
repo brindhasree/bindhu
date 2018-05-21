f447c8bc704d96f9bf6f8014c0e4c5fd057d8723
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-05-21 11:37:38 +0530

    Create countchar.cpp

---
+++ countchar.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char *array_point;
+    char c1;
+    int count=0,spcchr=0,count1;
+    char string_array[100];    
+    string str1;
+ 
+	
+//	cout << " Enter a string: ";
+    getline(cin, str1);
+    cout<<endl;
+    strcpy(string_array, str1.c_str());
+    for(array_point=string_array;*array_point!='\0';array_point++)
+    {
+        c1=*array_point;
+        count++;
+		if (isspace(c1))
+        
+            spcchr++;
+        
+    }
+       // cout <<" The number of characters in the string is: "<<count<<endl;      
+    
+       // cout<<" The number of spaces are: "<<spcchr<<endl;
+        count1=count-spcchr;
+        cout<<count1;
+              
+     return 0;
+}
